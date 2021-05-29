76bee4c593fe64331dfd78d3aae0a6f4b76bdcc8
Dimags500 <73039506+Dimags500@users.noreply.github.com>
2021-05-29 23:26:23 +0300

    swith check funct to 'ABC' base

--- script.js
+++ script.js
@@ -2,57 +2,65 @@
 
 let x = 'x';
 let o = 'o';
-var cell = [];
-let counterX = [];
-let counterO = [];
 
-document.querySelectorAll('.btns').forEach(item => {
-    item.addEventListener('click', event => {
-      if(item.innerHTML == ' ')
+arrX = "";
+arrO = "";
+let usedBoxes =[];
+
+// first checks if all boxes empty , if so adds first move as X and change background color
+// adds addEventListener for all boxes  
+document.querySelectorAll('.btns').forEach(box => {
+    box.addEventListener('click', event => {
+      if(box.innerHTML == ' ')
       {
-          if(cell.length == 0)
+          if(usedBoxes.length == 0)
           {
-            item.innerHTML = x;
-            var elem = document.getElementById(item.id) ;
-            var backColor = elem.style.backgroundColor = "cyan";
-            cell.push( { id :item.id ,value: item.innerHTML ,color : backColor }) ;
-            document.getElementById('info').innerHTML = 'X has started !'
-            counterX.push(item.id+0);
+            box.innerHTML = x;
+            var currBox = document.getElementById(box.id) ;
+            var backColor = box.style.backgroundColor = "cyan";
+            
+            usedBoxes.push( { id :currBox.id ,value: currBox.innerHTML ,color : backColor }) ;
+            document.getElementById('info').innerHTML = 'next move is O'
+            arrX += box.id;
 
           }
           else
-                check1(item);
+          {
+                check1(box);
                 check2();
             
       
-      }
+          }
+        }
     })
   })
 
-  function check1(item)
+  function check1(box)
   {
 
-    let last = cell.slice(-1).pop();
+    let last = usedBoxes.slice(-1).pop();
       
     if(last.value  == o )
     {
-        item.innerHTML = x;
-        var elem = document.getElementById(item.id) ;
+        box.innerHTML = x;
+        var elem = document.getElementById(box.id) ;
         var backColor = elem.style.backgroundColor = "cyan";
-        cell.push( { id :item.id ,value: item.innerHTML ,color : backColor }) ;
+        usedBoxes.push( { id :box.id ,value: box.innerHTML ,color : backColor }) ;
         document.getElementById('info').innerHTML = 'next move is O'
 
-        counterX.push(item.id+0);
+        arrX += box.id;
+
     }
     if(last.value == x )
     {
-        item.innerHTML = o;
-        var elem = document.getElementById(item.id) ;
+        box.innerHTML = o;
+        var elem = document.getElementById(box.id) ;
         var backColor = elem.style.backgroundColor = "orange";
-        cell.push( { id :item.id ,value: item.innerHTML ,color : backColor }) ;
+        usedBoxes.push( { id :box.id ,value: box.innerHTML ,color : backColor }) ;
         document.getElementById('info').innerHTML = 'next move is X'
 
-        counterO.push(item.id);
+        arrO += box.id;
+
     }
 
 
@@ -60,59 +68,68 @@ document.querySelectorAll('.btns').forEach(item => {
 
   function check2(){
 
-    if(counterX.length >= 3)
-    {
 
-        // for (a = 0; a < counterX.length; a++)
-        //  {
-            
-        //     for (b = 0; b < counterX.length; b++)
-        //     {
+  let arr = [arrX,arrO]; 
 
-        //         for (c = 0; c < counterX.length; c++)
-        //         {
+    for (i = 0; i < arr.length; i++){
+       
+        if((arr[i] == "ABC" || arr[i] == "ACB" || arr[i] == 'BCA' || arr[i] == 'BAC' || arr[i] == 'CBA' || arr[i] == 'CAB' )
+        || (arr[i] == 'DEF' || arr[i] == 'DFE' || arr[i] == 'EFD' || arr[i] == 'EDF' || arr[i] == 'FED' || arr[i] == 'FDE' )
+        || (arr[i] == 'GHI' || arr[i] == 'GIH' || arr[i] == 'HGI' || arr[i] == 'HIG' || arr[i] == 'IHG' || arr[i] == 'IGH' ) )
+        {
 
-        //             if(counterX[a] == 1 && counterX[b] || counterX[c]  )
-        //             {
+          if(arrO){
+            document.getElementById('info').innerHTML = " O is Win ";
+            return;
+          }
+          if(arrX){
+            document.getElementById('info').innerHTML = " X is Win ";
+            return;
 
-        //                 document.getElementById('info').innerHTML = 'X is Winner ';
-        //             }
-        //         }
+          }
 
-        //     }
-       // }
-
-       let x = 0 ;
-
-       for (i = 0; i < counterX.length; i++){
-           x = x+ parseInt(counterX[i]);
-       }
-       if (x == 60 ||x == 150 || x == 240 || x == 120 || x == 180 || x == 140  )
-       {
-        document.getElementById('info').innerHTML = ' Winner X';   
-       }
-           
+        }
+      
     }
-
-    if(counterO.length >= 3)
-    {
-
-   
-      let o = 0 ;
-
-       for (i = 0; i < counterO.length; i++){
-           o = o+ parseInt(counterO[i]);
-       }
-       if (o == 6 ||o == 15 || o == 24 || o == 12 || o == 18 || o == 14  ){
-        document.getElementById('info').innerHTML = ' Winner O ';   
-       }
-           
-    }
-
-    if(counterO.length > 9 || counterX.length >9){
-        document.getElementById('info').innerHTML = 'Noo is Winner ';   
-
-    }
-   
 
   }
+
+
+
+    // if(arrX.length >= 3)
+    // {
+
+    //    let x = 0 ;
+
+    //    for (i = 0; i < arrX.length; i++){
+    //        if(arr)
+    //   //  }
+    //   //  if (x == 60 ||x == 150 || x == 240 || x == 120 || x == 180 || x == 140  )
+    //   //  {
+    //     document.getElementById('info').innerHTML = ' Winner X';   
+    //    }
+           
+    // }
+
+    // if(arrO.length >= 3)
+    // {
+
+   
+    //   let o = 0 ;
+
+    //    for (i = 0; i < counterO.length; i++){
+    //        o = o+ parseInt(counterO[i]);
+    //    }
+    //    if (o == 6 ||o == 15 || o == 24 || o == 12 || o == 18 || o == 14  ){
+    //     document.getElementById('info').innerHTML = ' Winner O ';   
+    //    }
+           
+    // }
+
+    // if(counterO.length > 9 || counterX.length >9){
+    //     document.getElementById('info').innerHTML = 'Noo is Winner ';   
+
+    // }
+   
+
+  
